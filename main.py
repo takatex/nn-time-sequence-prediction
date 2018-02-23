81742bd0892a305f7c2c1daa643796265557a08f
takatex <astro.universe.takato@gmail.com>
2018-02-24 01:43:51 +0900

    add

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, sys
 sys.path.append(os.pardir)
 import numpy as np
 import pickle
-import datetime
+import time
 import argparse
 import matplotlib
 matplotlib.use('Agg')
@@ -39,39 +39,42 @@ if opt.gpu != 'None':
 # ----------
 DATA_PATH = './data/data.pkl'
 # DATA_PATH = './data/data2.pkl'
-RESULT_PATH = './result'
+RESULT_PATH = os.path.join('./result', opt.model)
 os.makedirs(RESULT_PATH, exist_ok=True)
 SEQ_LEN = 10
 INPUT_SIZE = 1 # The number of expected features in the input x
 HIDDEN_SIZE = 5 # The number of features in the hidden state h
-NUM_LAYERS = 2
+NUM_LAYERS = 1
 OUTPUT_SIZE = 1
 BATCH_SIZE = 200
-N_ITER = 1
+N_ITER = 10
 
 
 def models():
     if opt.model == 'rnn':
-        return RNN(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE, BATCH_SIZE)
+        return RNN(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE)
     elif opt.model == 'lstm':
-        return LSTM(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE, BATCH_SIZE)
+        return LSTM(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE)
     elif opt.model == 'qrnn':
         return QRNN(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE, use_cuda=opt.gpu)
     elif opt.model == 'cnn':
         return CNN(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
 
-def train(rawdata, data_num):
+def train(rawdata, i):
     datasets = DATASETS(SEQ_LEN, BATCH_SIZE, INPUT_SIZE, opt.model)
     X_train, y_train, X_test, y_test = datasets.make(rawdata)
     model = models()
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters())
-   
+
     # train
     # ----------
+    loss_history = []
+    time_history = []
     print("Train")
     for e in range(opt.epoch):
         total_loss = 0
+        start = time.time()
         for iter_ in range(N_ITER):
             x, y = datasets.mini_traindata(X_train, y_train)
             x = Variable(torch.from_numpy(x))
@@ -83,8 +86,12 @@ def train(rawdata, data_num):
             total_loss += loss.data[0]
             optimizer.step()
 
-        if (e+1) % 100 == 0:
-            print("epoch:\t{}\t loss:\t{}".format(e+1, loss.data[0]))
+            if (e+1) % 100 == 0:
+                print("epoch:\t{}\t loss:\t{}".format(e+1, loss.data[0]))
+
+        time_ = time.time() - start
+        loss_history.append(total_loss/N_ITER)
+        time_history.append(time_)
 
     # test
     # ----------
@@ -101,21 +108,46 @@ def train(rawdata, data_num):
     else:
         y_train_pred = y_train_pred[SEQ_LEN-1, :, :].reshape(-1)
         y_test_pred = y_test_pred[SEQ_LEN-1, :, :].reshape(-1)
+
     train_error, test_error = mse(y_train, y_train_pred, y_test, y_test_pred)
+
     plt.plot(y_test, color='blue')
     plt.plot(y_test_pred, color='red')
-    figname = 'data' + str(data_num) + '.png'
+    figname = 'data' + str(i) + '.png'
     plt.savefig(os.path.join(RESULT_PATH, figname))
     plt.close()
+
+    return loss_history, time_history, train_error, test_error
+
+
 
 def main():
     with open(DATA_PATH, 'rb') as f:
         data = pickle.load(f)
-    # rawdata = data
-    # train(rawdata, 100)
+    loss_history = []
+    time_history = []
+    train_error = []
+    test_error = []
     for i in range(10):
         rawdata = data[:, i]
-        train(rawdata, i)
+        i_loss_history, i_time_history, i_train_error, i_test_error = train(rawdata, i)
+        loss_history.append(i_loss_history)
+        time_history.append(i_time_history)
+        train_error.append(i_train_error)
+        test_error.append(i_test_error)
+
+    with open('./loss_history.pkl', 'wb') as f:
+        pickle.dump(loss_history, f)
+    with open('./time_history.pkl', 'wb') as f:
+        pickle.dump(time_history, f)
+    with open('./train_error.pkl', 'wb') as f:
+        pickle.dump(train_error, f)
+    with open('./test_error.pkl', 'wb') as f:
+        pickle.dump(test_error, f)
+
+    # for i in range(10):
+    #     rawdata = data[:, i]
+    #     train(rawdata, i)
 
 if __name__ == '__main__':
     main()
--- models/rnn.py
+++ models/rnn.py
@@ -3,18 +3,18 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, batch_size):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.batch_size = batch_size
+        # self.batch_size = batch_size
         # self.rnn = nn.RNN(input_size, hidden_size, self.num_layers)
         self.rnn = nn.RNN(input_size, hidden_size, num_layers)
         self.fc = nn.Linear(hidden_size, output_size)
     
     def forward(self, x, flag=False):
         # Forward propagate RNN
-        h = Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_size))
+        h = Variable(torch.zeros(self.num_layers, x.size(1), self.hidden_size))
         out, _ = self.rnn(x, h)
         
         # print(out.shape)
